77a7cd80cda2647df40824a608bbda81fd24daf3
pgr3931 <pascal.grones@gmx.at>
2023-12-08 00:39:38 +0100

    day 5/2

--- rust/src/day5/solution.rs
+++ rust/src/day5/solution.rs
@@ -37,10 +37,17 @@ pub fn solve(run_as: char) -> Result<(), io::Error> {
         |x, i| i > 1 && !x.ends_with(":"),
     );
 
-    let mut result = part1(seeds.clone(), maps.clone());
+    let mut sorted_maps = Vec::new();
+
+    for mut map in maps {
+        map.sort_by(|a, b| a.src_range_start.cmp(&b.src_range_start));
+        sorted_maps.push(map);
+    }
+
+    let mut result = part1(seeds.clone(), sorted_maps.clone());
     println!("Part 1: {}", result);
 
-    result = part2(seeds, maps);
+    result = part2_chunks(seeds, sorted_maps);
     println!("Part 2: {}", result);
 
     Ok(())
@@ -75,39 +82,65 @@ fn part1(seeds: Vec<i64>, maps: Vec<Vec<Instruction>>) -> i64 {
     result
 }
 
-fn part2(seeds: Vec<i64>, maps: Vec<Vec<Instruction>>) -> i64 {
-    let mut result = MAX;
+fn part2_chunks(seeds: Vec<i64>, maps: Vec<Vec<Instruction>>) -> i64 {
     let mut i = 0;
+    let mut chunks = Vec::new();
 
     while i < seeds.len() {
-        let min = cmp::min(seeds[i], seeds[i + 1]);
-        let max = cmp::max(seeds[i], seeds[i + 1]);
+        let min = seeds[i];
+        let max = seeds[i] + seeds[i + 1];
 
-        for seed in min..max {
-            let mut value = seed;
-
-            for map in &maps {
-                let instruction = map
-                    .iter()
-                    .find(|x| value >= x.src_range_start && value < x.src_range_start + x.range);
-
-                if instruction.is_none() {
-                    continue;
-                }
-
-                let offset =
-                    instruction.unwrap().dest_range_start - instruction.unwrap().src_range_start;
-
-                value += offset;
-            }
-
-            if value < result {
-                result = value;
-            }
-        }
+        chunks.push((min, max));
 
         i += 2;
     }
 
-    result
+    for map in &maps {
+        let mut mapped_chunks = Vec::new();
+
+        for (a, b) in &chunks {
+            let mut start_index = *a;
+
+            while start_index != *b {
+                let mut new_chunk;
+
+                let instruction = map.iter().find(|x| {
+                    start_index >= x.src_range_start && start_index < (x.src_range_start + x.range)
+                });
+
+                if instruction.is_some() {
+                    let inst = instruction.unwrap();
+
+                    new_chunk = (start_index, cmp::min(inst.src_range_start + inst.range, *b));
+
+                    let offset = inst.dest_range_start - inst.src_range_start;
+
+                    start_index = new_chunk.1;
+
+                    new_chunk.0 += offset;
+                    new_chunk.1 += offset;
+                } else {
+                    let instruction = map.iter().find(|x| x.src_range_start > start_index);
+
+                    if instruction.is_some() {
+                        let inst = instruction.unwrap();
+
+                        new_chunk = (start_index, cmp::min(inst.src_range_start, *b));
+                    } else {
+                        new_chunk = (start_index, *b);
+                    }
+
+                    start_index = new_chunk.1;
+                }
+
+                mapped_chunks.push(new_chunk);
+            }
+        }
+
+        chunks = mapped_chunks;
+    }
+
+    chunks.sort_by(|a, b| a.0.cmp(&b.0));
+
+    chunks[0].0
 }
