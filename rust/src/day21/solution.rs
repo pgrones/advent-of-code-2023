b4883cb82f53a14845139c88a9bfc3578cc7bf12
pgr3931 <pascal.grones@gmx.at>
2024-01-07 18:01:46 +0100

    day 21/2 unfinished
    Co-authored-by: Jonas Matt <jmatt@student.ethz.ch>

--- rust/src/day21/solution.rs
+++ rust/src/day21/solution.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashSet,
-    fs::File,
-    io::{self, BufReader, Lines},
+    io::{self},
     vec,
 };
 
@@ -20,13 +19,13 @@ pub fn solve(run_as: char) -> Result<(), io::Error> {
     println!("Part 1: {}", count);
 
     // let lines2 = read_lines_iterable(input_file)?;
-    // count = part2(lines2);
-    // println!("Part 2: {}", count);
+    count = part2(&garden);
+    println!("Part 2: {}", count);
 
     Ok(())
 }
 
-fn part1(garden: &Vec<Vec<char>>) -> u32 {
+fn part1(garden: &Vec<Vec<char>>) -> u64 {
     let mut start = (0, 0);
 
     'outer: for y in 0..garden.len() {
@@ -38,50 +37,65 @@ fn part1(garden: &Vec<Vec<char>>) -> u32 {
         }
     }
 
-    let mut queue = vec![start];
+    let mut queue: Vec<((usize, usize), bool)> = vec![(start, false)];
 
     for _ in 0..64 {
         let mut next_positions = HashSet::new();
 
         while queue.len() > 0 {
-            next_positions.extend(execute_step(garden, queue.pop().unwrap()));
+            next_positions.extend(execute_step(garden, queue.pop().unwrap(), true));
         }
 
-        queue = Vec::from_iter(next_positions)
+        queue = Vec::from_iter(next_positions);
     }
 
-    return queue.len() as u32;
+    return queue
+        .iter()
+        .filter(|((x, y), a)| !a && y.abs_diff(65) + x.abs_diff(65) <= 65)
+        .count() as u64;
 }
 
-fn execute_step(garden: &Vec<Vec<char>>, curr_pos: (usize, usize)) -> Vec<(usize, usize)> {
+fn execute_step(
+    garden: &Vec<Vec<char>>,
+    curr_pos: ((usize, usize), bool),
+    wrap: bool,
+) -> Vec<((usize, usize), bool)> {
     let mut result = vec![];
 
-    if curr_pos.1 > 0 && garden[curr_pos.1 - 1][curr_pos.0] != '#' {
-        result.push((curr_pos.0, curr_pos.1 - 1));
+    if curr_pos.0 .1 > 0 && garden[curr_pos.0 .1 - 1][curr_pos.0 .0] != '#' {
+        result.push(((curr_pos.0 .0, curr_pos.0 .1 - 1), curr_pos.1));
+    } else if curr_pos.0 .1 == 0 && wrap {
+        result.push(((curr_pos.0 .0, garden.len() - 1), !curr_pos.1));
     }
 
-    if curr_pos.1 < garden.len() - 1 && garden[curr_pos.1 + 1][curr_pos.0] != '#' {
-        result.push((curr_pos.0, curr_pos.1 + 1));
+    if curr_pos.0 .1 < garden.len() - 1 && garden[curr_pos.0 .1 + 1][curr_pos.0 .0] != '#' {
+        result.push(((curr_pos.0 .0, curr_pos.0 .1 + 1), curr_pos.1));
+    } else if curr_pos.0 .1 == garden.len() - 1 && wrap {
+        result.push(((curr_pos.0 .0, 0), !curr_pos.1));
     }
 
-    if curr_pos.0 > 0 && garden[curr_pos.1][curr_pos.0 - 1] != '#' {
-        result.push((curr_pos.0 - 1, curr_pos.1));
+    if curr_pos.0 .0 > 0 && garden[curr_pos.0 .1][curr_pos.0 .0 - 1] != '#' {
+        result.push(((curr_pos.0 .0 - 1, curr_pos.0 .1), curr_pos.1));
+    } else if curr_pos.0 .0 == 0 && wrap {
+        result.push(((garden[0].len() - 1, curr_pos.0 .1), !curr_pos.1));
     }
 
-    if curr_pos.0 < garden[0].len() - 1 && garden[curr_pos.1][curr_pos.0 + 1] != '#' {
-        result.push((curr_pos.0 + 1, curr_pos.1));
+    if curr_pos.0 .0 < garden[0].len() - 1 && garden[curr_pos.0 .1][curr_pos.0 .0 + 1] != '#' {
+        result.push(((curr_pos.0 .0 + 1, curr_pos.0 .1), curr_pos.1));
+    } else if curr_pos.0 .0 == garden[0].len() - 1 && wrap {
+        result.push(((0, curr_pos.0 .1), !curr_pos.1));
     }
 
     result
 }
 
-// fn render_garden(garden: &Vec<Vec<char>>, plots: &Vec<(usize, usize)>) {
-//     for y in 0..garden.len() {
-//         for x in 0..garden[0].len() {
-//             if plots.contains(&(x, y)) {
+// fn render_garden(garden: &Vec<Vec<char>>, plots: &Vec<(usize, usize)>, multiplicity: usize) {
+//     for y in 0..multiplicity * garden.len() {
+//         for x in 0..multiplicity * garden[0].len() {
+//             if plots.contains(&(x % garden.len(), y % garden[0].len())) {
 //                 print!("O");
 //             } else {
-//                 print!("{}", garden[y][x]);
+//                 print!("{}", garden[y % garden[0].len()][x % garden.len()]);
 //             }
 //         }
 //         println!();
@@ -89,6 +103,113 @@ fn execute_step(garden: &Vec<Vec<char>>, curr_pos: (usize, usize)) -> Vec<(usize
 //     println!();
 // }
 
-fn part2(lines: Lines<BufReader<File>>) -> u32 {
-    0
+const STEPS: u64 = 26501365;
+
+fn part2(garden: &Vec<Vec<char>>) -> u64 {
+    let center = garden.len() / 2;
+
+    let remaining_steps = (STEPS - center as u64) % garden.len() as u64 + garden.len() as u64;
+    let edge = (STEPS - center as u64) / garden.len() as u64 - 1;
+
+    let edge_length = 1 + edge * 2;
+
+    // let number_of_gardens = edge_length * edge_length / 2;
+    // let number_of_inside_diamonds = number_of_gardens + 1;
+    // let number_of_outside_diamonds = number_of_gardens;
+
+    // 3632
+    // 3701
+
+    let mut diamond_indexes = vec![];
+    let mut number_of_plots = 0;
+    let mut number_of_plots_inside_diamond = 0;
+    let mut number_of_plots_odd = 0;
+    let mut number_of_plots_inside_diamond_odd = 0;
+
+    for y in 0..garden.len() {
+        for x in 0..garden[0].len() {
+            if y.abs_diff(center) + x.abs_diff(center) == center {
+                diamond_indexes.push((x, y));
+            }
+
+            if (x + y) % 2 == 1 && (garden[y][x] == '.' || garden[y][x] == 'S') {
+                number_of_plots += 1;
+
+                if y.abs_diff(center) + x.abs_diff(center) <= center {
+                    number_of_plots_inside_diamond += 1;
+                }
+            }
+
+            if (x + y) % 2 == 0 && (garden[y][x] == '.' || garden[y][x] == 'S') {
+                number_of_plots_odd += 1;
+
+                if y.abs_diff(center) + x.abs_diff(center) <= center {
+                    number_of_plots_inside_diamond_odd += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", number_of_plots_inside_diamond);
+
+    let number_of_plots_outside_diamond = number_of_plots - number_of_plots_inside_diamond;
+    let number_of_plots_outside_diamond_odd =
+        number_of_plots_odd - number_of_plots_inside_diamond_odd;
+
+    let mut odd_multiplier = 1;
+
+    for i in (5..edge_length + 1).step_by(4) {
+        odd_multiplier += 4 * (i - 1);
+    }
+
+    let mut even_multiplier = 0;
+
+    for i in (3..edge_length + 1).step_by(4) {
+        even_multiplier += 4 * (i - 1);
+    }
+
+    let number_of_plots_inside_fat_ass_diamond = even_multiplier / 2
+        * number_of_plots_inside_diamond
+        + (odd_multiplier / 2 + 1) * number_of_plots_inside_diamond_odd
+        + even_multiplier / 2 * number_of_plots_outside_diamond
+        + odd_multiplier / 2 * number_of_plots_outside_diamond_odd;
+
+    let mut queue: Vec<((usize, usize), bool)> =
+        diamond_indexes.iter().map(|x| (*x, false)).collect();
+
+    for _ in 0..remaining_steps {
+        let mut next_positions = HashSet::new();
+
+        while queue.len() > 0 {
+            next_positions.extend(execute_step(garden, queue.pop().unwrap(), true));
+        }
+
+        queue = Vec::from_iter(next_positions)
+    }
+
+    let reachable = queue
+        .iter()
+        .filter(|&((x, y), wrapped)| *wrapped || y.abs_diff(center) + x.abs_diff(center) > center)
+        .map(|x| x.0)
+        .count() as u64;
+
+    let result = number_of_plots_inside_fat_ass_diamond + reachable * edge_length;
+
+    println!(
+        "number_of_plots_inside_fat_ass_diamond: {}",
+        number_of_plots_inside_fat_ass_diamond
+    );
+    println!("reachable: {}", reachable);
+    println!("edge_length: {}", edge_length);
+    println!("reachable * edge_length: {}", reachable * edge_length);
+
+    result
 }
+
+// 600336060511101 (correct)
+// 600661970655197
+// 596405782573597
+// 602094355805197
+// 599231058260303
+//      4465559163
+//     81850580000
--- rust/src/day23/solution.rs
+++ rust/src/day23/solution.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code, unused_imports)]
 
 use std::{
+    collections::HashSet,
     fs::File,
     io::{self, BufReader, Lines},
     thread, time,
@@ -22,14 +23,14 @@ pub fn solve(run_as: char) -> Result<(), io::Error> {
     // We need a bigger stack for this
     let child = thread::Builder::new()
         .stack_size(STACK_SIZE)
-        .spawn(move || println!("Part 1: {}", part1(&hiking_map)))
+        .spawn(move || {
+            println!("Part 1: {}", part1(&hiking_map));
+            println!("Part 2: {}", part2(&hiking_map));
+        })
         .unwrap();
 
     // Wait for thread to join
     child.join().unwrap();
-
-    // count = part2(lines2);
-    // println!("Part 2: {}", count);
 
     Ok(())
 }
@@ -63,7 +64,7 @@ fn traverse(
     // render_map(hiking_map, curr_pos, steps);
     // thread::sleep(time::Duration::from_millis(200));
 
-    let paths = get_next_paths(hiking_map, last_pos, curr_pos);
+    let paths = get_next_paths(hiking_map, last_pos, curr_pos, true);
 
     let steps_copy = steps.clone();
 
@@ -83,13 +84,14 @@ fn get_next_paths(
     hiking_map: &Vec<Vec<char>>,
     last_pos: (usize, usize),
     curr_pos: (usize, usize),
+    is_icy: bool,
 ) -> Vec<(usize, usize)> {
     let mut result = vec![];
 
     if curr_pos.1 > 0
         && (curr_pos.0, curr_pos.1 - 1) != last_pos
         && hiking_map[curr_pos.1 - 1][curr_pos.0] != '#'
-        && hiking_map[curr_pos.1 - 1][curr_pos.0] != 'v'
+        && (!is_icy || hiking_map[curr_pos.1 - 1][curr_pos.0] != 'v')
     {
         result.push((curr_pos.0, curr_pos.1 - 1));
     }
@@ -97,7 +99,7 @@ fn get_next_paths(
     if curr_pos.1 < hiking_map.len() - 1
         && (curr_pos.0, curr_pos.1 + 1) != last_pos
         && hiking_map[curr_pos.1 + 1][curr_pos.0] != '#'
-        && hiking_map[curr_pos.1 + 1][curr_pos.0] != '^'
+        && (!is_icy || hiking_map[curr_pos.1 + 1][curr_pos.0] != '^')
     {
         result.push((curr_pos.0, curr_pos.1 + 1));
     }
@@ -105,7 +107,7 @@ fn get_next_paths(
     if curr_pos.0 > 0
         && (curr_pos.0 - 1, curr_pos.1) != last_pos
         && hiking_map[curr_pos.1][curr_pos.0 - 1] != '#'
-        && hiking_map[curr_pos.1][curr_pos.0 - 1] != '>'
+        && (!is_icy || hiking_map[curr_pos.1][curr_pos.0 - 1] != '>')
     {
         result.push((curr_pos.0 - 1, curr_pos.1));
     }
@@ -113,7 +115,7 @@ fn get_next_paths(
     if curr_pos.0 < hiking_map[0].len() - 1
         && (curr_pos.0 + 1, curr_pos.1) != last_pos
         && hiking_map[curr_pos.1][curr_pos.0 + 1] != '#'
-        && hiking_map[curr_pos.1][curr_pos.0 + 1] != '<'
+        && (!is_icy || hiking_map[curr_pos.1][curr_pos.0 + 1] != '<')
     {
         result.push((curr_pos.0 + 1, curr_pos.1));
     }
@@ -135,6 +137,52 @@ fn render_map(hiking_map: &Vec<Vec<char>>, curr_pos: (usize, usize), steps: &Vec
     println!("{:?}", steps);
 }
 
-fn part2(lines: Lines<BufReader<File>>) -> u32 {
-    0
+fn part2(hiking_map: &Vec<Vec<char>>) -> u32 {
+    let start = (1usize, 1usize);
+    let last_pos = (1usize, 0usize);
+    let end = (hiking_map[0].len() - 2, hiking_map.len() - 1);
+
+    let mut steps = vec![(0, HashSet::from([last_pos]))];
+
+    traverse2(hiking_map, last_pos, start, end, &mut steps, 0);
+
+    steps.iter().map(|x| x.0).max().unwrap()
+}
+
+fn traverse2(
+    hiking_map: &Vec<Vec<char>>,
+    last_pos: (usize, usize),
+    curr_pos: (usize, usize),
+    end_pos: (usize, usize),
+    steps: &mut Vec<(u32, HashSet<(usize, usize)>)>,
+    step_pos: usize,
+) {
+    steps[step_pos].0 += 1;
+
+    if !steps[step_pos].1.insert(curr_pos) {
+        steps.remove(step_pos);
+        return;
+    }
+
+    if curr_pos == end_pos {
+        return;
+    }
+
+    // render_map(hiking_map, curr_pos, steps);
+    // thread::sleep(time::Duration::from_millis(200));
+
+    let paths = get_next_paths(hiking_map, last_pos, curr_pos, false);
+
+    let steps_copy = steps.clone();
+
+    for i in 0..paths.len() {
+        let mut pos = step_pos;
+
+        if i > 0 {
+            steps.push(steps_copy[step_pos].clone());
+            pos = steps.len() - 1;
+        }
+
+        traverse2(hiking_map, curr_pos, paths[i], end_pos, steps, pos);
+    }
 }
